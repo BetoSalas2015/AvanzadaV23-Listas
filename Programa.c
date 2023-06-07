ed9e019acc3c30287c57c514011605a3cff2ca39
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2023-06-07 12:36:08 -0600

    Sesión 9-10 - 07/06/2023

--- Programa.c
+++ Programa.c
@@ -9,13 +9,66 @@ struct Nodo
 {
 	char info;
 	struct Nodo *sig;
-};
+};								// Creando un nuevo tipo de dato
 
-typedef struct Nodo nodo;
+typedef struct Nodo nodo;		//  Se define formalemnte como tipo de dato
+
+nodo *raiz = NULL;				// Se define una lista vacía
+
+//  Prototipos de Función 
+void insert(char dato);
+void imprimeLista();
+
+void insert(char dato)
+{
+	nodo *nuevo = NULL;			//  Apuntador para el nuevo nodo
+	nuevo = (nodo *) malloc(sizeof (nodo) );		// reservo un nuevo nodo
+	if( nuevo == NULL)			//  Hay memoria disponible?
+	{
+		printf("ERROR: No hay memoria disponible");		// No hay memoria
+		pausa;
+		exit(1);		//  Termina el programa con condición de error 1 (no hay memoria)
+	}
+	nuevo -> info = dato;	// Preservamos el dato recibido en el nodo
+	nuevo -> sig = NULL;	// El apuntador sigueinte se apunta NULL	
+	if( raiz == NULL)		// La lista está vacía?
+	{						//  Si esta vacía
+		raiz = nuevo;
+	}
+	else					//  No está vacía
+	{				
+		nodo *recorre = raiz;	//  Me posiciono en el primer nodo de la lista
+		while( recorre -> sig != NULL)		// Es el ultimo nodo
+		{									// No.
+			recorre = recorre-> sig;		// Me muevo al siguente nodo
+		}
+		recorre ->sig = nuevo;				// Inserto al final el nuevo nodo
+	}
+}
+
+void imprimeLista()
+{
+	nodo *recorre = raiz;	//  Me posiciono en el primer nodo de la lista
+	while( recorre != NULL)		// Se acabó la lista?
+	{									// No.
+		printf("%c, ", recorre->info);
+		recorre = recorre-> sig;		// Me muevo al siguente nodo
+	}
+	putchar('\n');
+}
+
 
 int main()
 {
-	nodo variable;
+	insert('R');
+	insert('o');
+	insert('b');
+	insert('e');
+	insert('r');
+	insert('t');
+	insert('o');
+
+	imprimeLista();
 	pausa;
 	return 0;
 }
